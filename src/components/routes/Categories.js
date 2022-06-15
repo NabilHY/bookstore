54c23db7bc274e06eef02e670329c77e7a8ac7e4
nabsternab <hayounn.etd@gmail.com>
2022-06-15 20:43:57 +0100

    Add check status functionnality

--- src/components/routes/Categories.js
+++ src/components/routes/Categories.js
@@ -1,7 +1,20 @@
-const Categories = () => (
-  <div>
-    <button type="button">Check status</button>
-  </div>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
+
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const clickHandler = () => {
+    dispatch(checkStatus());
+  };
+  return (
+    <div>
+      <button onClick={clickHandler} type="button">Check status</button>
+      <h1>
+        {status}
+      </h1>
+    </div>
+  );
+};
 
 export default Categories;
