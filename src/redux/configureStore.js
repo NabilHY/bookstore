c3e9b2f2505df03211bfcdf28300c46a1abee4a4
nabsternab <hayounn.etd@gmail.com>
2022-06-16 20:24:34 +0100

    install redux thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import categoriesReducer from './categories/categories';
 import bookReducer from './books/books';
 
@@ -7,6 +8,6 @@ const reducers = {
   categories: categoriesReducer,
 };
 
-const store = createStore(combineReducers(reducers));
+const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
 
 export default store;
