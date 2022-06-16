08f9366e11c70a549f052ee21fe289d9547322fa
nabsternab <hayounn.etd@gmail.com>
2022-06-16 22:31:40 +0100

    Add an API display functionnality

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,39 +1,42 @@
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Wr1w5pRVh0VMaUEtfgxC/books/';
 const ADD_BOOK = 'books/ADD_BOOK';
 const REMOVE_BOOK = 'books/REMOVE_BOOK';
+const DISPLAY_BOOKS = 'books/DISPLAY_BOOKS';
 
-const initialeState = [
-  {
-    title: 'Awaken the giant within',
-    author: 'Anthony robbins',
-    id: uuidv4(),
-  },
-  {
-    title: 'The power of now',
-    author: 'Echart Tolle',
-    id: uuidv4(),
-  },
-  {
-    title: 'Fault in our stars',
-    author: 'John Green',
-    id: uuidv4(),
-  },
-];
+const initialeState = [];
 
-export function addBook(newBook) {
-  return {
-    type: ADD_BOOK,
-    payload: newBook,
-  };
-}
+export const addBook = (newBook) => ({
+  type: ADD_BOOK,
+  payload: newBook,
+});
 
-export function removeBook(removedBook) {
-  return {
-    type: REMOVE_BOOK,
-    payload: removedBook,
-  };
-}
+export const removeBook = (removedBook) => ({
+  type: REMOVE_BOOK,
+  payload: removedBook,
+});
+
+export const displayBooks = (books) => ({
+  type: DISPLAY_BOOKS,
+  payload: books,
+});
+
+export const displayAllBooks = () => (dispatch) => {
+  axios.get(URL)
+    .then((res) => res.data)
+    .then((data) => {
+      const allBooks = Object.keys(data).map((key) => {
+        const book = data[key][0];
+        return {
+          id: key,
+          ...book,
+        };
+      });
+      dispatch(displayBooks(allBooks));
+    });
+};
 
 const bookReducer = (state = initialeState, action) => {
   switch (action.type) {
@@ -44,6 +47,8 @@ const bookReducer = (state = initialeState, action) => {
       ];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
+    case DISPLAY_BOOKS:
+      return [...action.payload];
     default:
       return state;
   }
