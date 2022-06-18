9cd45dc412d98f128677e000fcea1e24f8f3c484
nabsternab <hayounn.etd@gmail.com>
2022-06-18 02:10:30 +0100

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
-    <Link to="/">Books</Link>
-    <Link to="/categories">Categories</Link>
+  <nav className="navbar">
+    <span className="Bookstore-CMS">
+      Bookstore CMS
+    </span>
+    <Link class="navlink" to="/">Books</Link>
+    <Link class="navlink" to="/categories">Categories</Link>
+    <div className="Oval">
+      <div className="Mask" />
+    </div>
   </nav>
 );
 
